f80c730e6a0a37d209065a162a28db53412bb75e
JWeinmann <weinmann@uleth.ca>
2019-01-31 14:00:10 -0700

    completely cleared all files

--- Tree.h
+++ Tree.h
@@ -16,29 +16,13 @@ class Tree{
         Tree();
         virtual ~Tree(){};
         void addValue(int);
-        Node* insertNode(Node *, Node*);
-        void insertFix(Node *);
-        void deleteValue(int);
-        Node* findNode(int);
-        Node* findNode(Node*, int);
-        Node* findMin(Node*);
-        Node* findMax(Node*);
-        void deleteFix(Node *);
-        int getColour(Node *);
-        void transplant(Node*, Node*);
-        void setColour(Node *, int);
-        void leftRotation(Node *);
-        void rightRotation(Node *);
-        void walk();
-        void print(Node*, int);
-
-
 
 
 
 
     private:
         Node* root;
+        Node* nil;
 
 };
 
